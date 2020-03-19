d5383e46e27a56e73043241a52ab5021c3bbb6cd
kellypowers <kpowers.nj@gmail.com>
2020-03-19 16:53:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  get 'song/create'
+
+  get 'song/update'
+
+  get 'song/show'
+
+  get 'genre/create'
+
+  get 'genre/update'
+
+  get 'genre/show'
+
+  get 'artist/create'
+
+  get 'artist/update'
+
+  get 'artist/show'
+
+  resources :artists
+  resources :songs
+  resources :genres
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
